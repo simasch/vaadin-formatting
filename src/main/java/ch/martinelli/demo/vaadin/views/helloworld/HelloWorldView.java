399a70967423af17afc2401489adfba5b9702e00
Simon Martinelli <simon@72.services>
2022-07-28 10:37:24 +0200

    Added binder

--- src/main/java/ch/martinelli/demo/vaadin/views/helloworld/HelloWorldView.java
+++ src/main/java/ch/martinelli/demo/vaadin/views/helloworld/HelloWorldView.java
@@ -1,35 +1,59 @@
 package ch.martinelli.demo.vaadin.views.helloworld;
 
 import ch.martinelli.demo.vaadin.views.MainLayout;
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.Result;
+import com.vaadin.flow.data.binder.ValueContext;
+import com.vaadin.flow.data.converter.Converter;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 @PageTitle("Hello World")
 @Route(value = "hello", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
-public class HelloWorldView extends HorizontalLayout {
+public class HelloWorldView extends VerticalLayout {
 
-    private TextField name;
-    private Button sayHello;
+    private final DecimalFormat decimalFormate = new DecimalFormat("###,###,###.00");
 
     public HelloWorldView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
+        Binder<Person> binder = new Binder<>(Person.class);
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        TextField name = new TextField("Name");
+        binder.forField(name)
+                .bind("name");
 
-        add(name, sayHello);
+        TextField salary = new TextField("Salary");
+        binder.forField(salary)
+                .withConverter(new Converter<String, BigDecimal>() {
+                    @Override
+                    public Result<BigDecimal> convertToModel(String value, ValueContext context) {
+                        if (value == null || value.length() == 0) {
+                            return Result.ok(new BigDecimal(0));
+                        } else {
+                            return Result.ok(new BigDecimal(value));
+                        }
+                    }
+
+                    @Override
+                    public String convertToPresentation(BigDecimal value, ValueContext context) {
+                        if (value == null) {
+                            return "";
+                        } else {
+                            return decimalFormate.format(value);
+                        }
+                    }
+                })
+                .bind("salary");
+
+        binder.setBean(new Person());
+
+        add(name, salary);
     }
 
 }
---
+++ src/main/java/ch/martinelli/demo/vaadin/views/helloworld/Person.java
@@ -0,0 +1,25 @@
+package ch.martinelli.demo.vaadin.views.helloworld;
+
+import java.math.BigDecimal;
+
+public class Person {
+
+    private String name;
+    private BigDecimal salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+}
